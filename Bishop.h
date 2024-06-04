53df1ec20a52edf5efcb23399f06f034fa681861
jehhuilee <dlwpgml56@naver.com>
2024-06-04 19:26:38 +0900

    비숍이 다른 기물을 잡았을때

---
+++ Bishop.h
@@ -0,0 +1,176 @@
+#pragma once
+
+#include "basic.h"
+
+
+
+int BishopWhite(int beforeX, int beforeY, int afterX, int afterY)
+{
+	int j = beforeX - 1;
+	for (int i = beforeY - 1; i >= 0; i--)
+	{
+		if (board[i][j] >= 0 && (afterY == i && afterX == j))
+		{
+			return 1;
+		}
+		else if (board[i][j] != 0)
+		{
+			break;
+		}
+		j--;
+	}
+	j = beforeX + 1;
+	for (int i = beforeY - 1; i >= 0; i--)
+	{
+		if (board[i][j] >= 0 && (afterY == i && afterX == j))
+		{
+			return 1;
+		}
+		else if (board[i][j] != 0)
+		{
+			break;
+		}
+		j++;
+	}
+	j = beforeX - 1;
+	for (int i = beforeY + 1; i <= 7; i++)
+	{
+		if (board[i][j] >= 0 && (afterY == i && afterX == j))
+		{
+			return 1;
+		}
+		else if (board[i][j] != 0)
+		{
+			break;
+		}
+		j--;
+	}
+	j = beforeX + 1;
+	for (int i = beforeY + 1; i <= 7; i++)
+	{
+		if (board[i][j] >= 0 && (afterY == i && afterX == j))
+		{
+			return 1;
+		}
+		else if (board[i][j] != 0)
+		{
+			break;
+		}
+		j++;
+	}
+	return 0;
+}
+
+int BishopBlack(int beforeX, int beforeY, int afterX, int afterY)
+{
+	int j = beforeX - 1;
+	for (int i = beforeY - 1; i >= 0; i--)
+	{
+		if (board[i][j] <= 0 && (afterY == i && afterX == j))
+		{
+			return 1;
+		}
+		else if (board[i][j] != 0)
+		{
+			break;
+		}
+		j--;
+	}
+	j = beforeX + 1;
+	for (int i = beforeY - 1; i >= 0; i--)
+	{
+		if (board[i][j] <= 0 && (afterY == i && afterX == j))
+		{
+			return 1;
+		}
+		else if (board[i][j] != 0)
+		{
+			break;
+		}
+		j++;
+	}
+	j = beforeX - 1;
+	for (int i = beforeY + 1; i <= 7; i++)
+	{
+		if (board[i][j] <= 0 && (afterY == i && afterX == j))
+		{
+			return 1;
+		}
+		else if (board[i][j] != 0)
+		{
+			break;
+		}
+		j--;
+	}
+	j = beforeX + 1;
+	for (int i = beforeY + 1; i <= 7; i++)
+	{
+		if (board[i][j] <= 0 && (afterY == i && afterX == j))
+		{
+			return 1;
+		}
+		else if (board[i][j] != 0)
+		{
+			break;
+		}
+		j++;
+	}
+	return 0;
+}
+
+int BishopWhiteCatch(int beforeX, int beforeY, int afterX, int afterY)
+{
+	int j = beforeX - 1;
+	for (int i = beforeY - 1; i >= 0; i--)
+	{
+		if (board[i][j] >= 0 && (afterY == i && afterX == j))
+		{
+			return 1;
+		}
+		else if (board[i][j] != 0)
+		{
+			break;
+		}
+		j--;
+	}
+	j = beforeX + 1;
+	for (int i = beforeY - 1; i >= 0; i--)
+	{
+		if (board[i][j] >= 0 && (afterY == i && afterX == j))
+		{
+			return 1;
+		}
+		else if (board[i][j] != 0)
+		{
+			break;
+		}
+		j++;
+	}
+	j = beforeX - 1;
+	for (int i = beforeY + 1; i <= 7; i++)
+	{
+		if (board[i][j] >= 0 && (afterY == i && afterX == j))
+		{
+			return 1;
+		}
+		else if (board[i][j] != 0)
+		{
+			break;
+		}
+		j--;
+	}
+	j = beforeX + 1;
+	for (int i = beforeY + 1; i <= 7; i++)
+	{
+		if (board[i][j] >= 0 && (afterY == i && afterX == j))
+		{
+			return 1;
+		}
+		else if (board[i][j] != 0)
+		{
+			break;
+		}
+		j++;
+	}
+	return 0;
+}
