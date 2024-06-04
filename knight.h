4ca34f34c6152901ae6388d5adc6ed36a7a59ad5
ossys0920 <ossys0920@gmail.com>
2024-06-04 20:35:26 +0900

    Knight 다른 기물 잡을 때

--- knight.h
+++ knight.h
@@ -74,3 +74,76 @@ int KnightBlack(int oldx, int oldy, int newx, int newy)
 	return 0;
 }
 
+int KnightWhiteCatch(int oldx, int oldy, int newx, int newy)
+{
+	if (oldy - 2 >= 0 && oldx - 1 >= 0 && newy == oldy - 2 && newx == oldx - 1 && board[newy][newx] >= 0)
+	{
+		return 1; 
+	}
+	if (oldy - 2 >= 0 && oldx + 1 < BOARDLEN && newy == oldy - 2 && newx == oldx + 1 && board[newy][newx] >= 0)
+	{
+		return 1; 
+	}
+	if (oldy - 1 >= 0 && oldx + 2 < BOARDLEN && newy == oldy - 1 && newx == oldx + 2 && board[newy][newx] >= 0)
+	{
+		return 1; 
+	}
+	if (oldy + 1 >= 0 && oldx + 2 < BOARDLEN && newy == oldy + 1 && newx == oldx + 2 && board[newy][newx] >= 0)
+	{
+		return 1; 
+	}
+	if (oldy + 2 < BOARDLEN && oldx + 1 < BOARDLEN && newy == oldy + 2 && newx == oldx + 1 && board[newy][newx] >= 0)
+	{
+		return 1; 
+	}
+	if (oldy + 2 < BOARDLEN && oldx - 1 >= 0 && newy == oldy + 2 && newx == oldx - 1 && board[newy][newx] >= 0)
+	{
+		return 1; 
+	}
+	if (oldy + 1 < BOARDLEN && oldx - 2 >= 0 && newy == oldy + 1 && newx == oldx - 2 && board[newy][newx] >= 0)
+	{
+		return 1; 
+	}
+	if (oldy - 1 >= 0 && oldx - 2 >= 0 && newy == oldy - 1 && newx == oldx - 2 && board[newy][newx] >= 0)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+int KnightBlackCatch(int oldx, int oldy, int newx, int newy)
+{
+	if (oldy - 2 >= 0 && oldx - 1 >= 0 && newy == oldy - 2 && newx == oldx - 1 && board[newy][newx] <= 0)
+	{
+		return 1;
+	}
+	if (oldy - 2 >= 0 && oldx + 1 < BOARDLEN && newy == oldy - 2 && newx == oldx + 1 && board[newy][newx] <= 0)
+	{
+		return 1; 
+	}
+	if (oldy - 1 >= 0 && oldx + 2 < BOARDLEN && newy == oldy - 1 && newx == oldx + 2 && board[newy][newx] <= 0)
+	{
+		return 1; 
+	}
+	if (oldy + 1 >= 0 && oldx + 2 < BOARDLEN && newy == oldy + 1 && newx == oldx + 2 && board[newy][newx] <= 0)
+	{
+		return 1; 
+	}
+	if (oldy + 2 < BOARDLEN && oldx + 1 < BOARDLEN && newy == oldy + 2 && newx == oldx + 1 && board[newy][newx] <= 0)
+	{
+		return 1; 
+	}
+	if (oldy + 2 < BOARDLEN && oldx - 1 >= 0 && newy == oldy + 2 && newx == oldx - 1 && board[newy][newx] <= 0)
+	{
+		return 1; 
+	}
+	if (oldy + 1 < BOARDLEN && oldx - 2 >= 0 && newy == oldy + 1 && newx == oldx - 2 && board[newy][newx] <= 0)
+	{
+		return 1; 
+	}
+	if (oldy - 1 >= 0 && oldx - 2 >= 0 && newy == oldy - 1 && newx == oldx - 2 && board[newy][newx] <= 0)
+	{
+		return 1;
+	}
+	return 0;
+}
