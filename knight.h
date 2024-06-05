3f4d9f1709724db02c0a2b4ecc46bceec5ce57c5
ossys0920 <ossys0920@gmail.com>
2024-06-05 09:16:57 +0900

    knight 다른 기물잡을 때 추가 수정

--- knight.h
+++ knight.h
@@ -74,74 +74,74 @@ int KnightBlack(int oldx, int oldy, int newx, int newy)
 	return 0;
 }
 
-int KnightWhiteCatch(int oldx, int oldy, int newx, int newy)
+int KnightWhiteCatch(int oldx, int oldy, int catchx, int catchy)
 {
-	if (oldy - 2 >= 0 && oldx - 1 >= 0 && newy == oldy - 2 && newx == oldx - 1 && board[newy][newx] >= 0)
+	if (oldy - 2 >= 0 && oldx - 1 >= 0 && catchy == oldy - 2 && catchx == oldx - 1 && board[catchy][catchx] >= 0)
 	{
 		return 1; 
 	}
-	if (oldy - 2 >= 0 && oldx + 1 < BOARDLEN && newy == oldy - 2 && newx == oldx + 1 && board[newy][newx] >= 0)
+	if (oldy - 2 >= 0 && oldx + 1 < BOARDLEN && catchy == oldy - 2 && catchx == oldx + 1 && board[catchy][catchx] >= 0)
 	{
 		return 1; 
 	}
-	if (oldy - 1 >= 0 && oldx + 2 < BOARDLEN && newy == oldy - 1 && newx == oldx + 2 && board[newy][newx] >= 0)
+	if (oldy - 1 >= 0 && oldx + 2 < BOARDLEN && catchy == oldy - 1 && catchx == oldx + 2 && board[catchy][catchx] >= 0)
 	{
 		return 1; 
 	}
-	if (oldy + 1 >= 0 && oldx + 2 < BOARDLEN && newy == oldy + 1 && newx == oldx + 2 && board[newy][newx] >= 0)
+	if (oldy + 1 >= 0 && oldx + 2 < BOARDLEN && catchy == oldy + 1 && catchx == oldx + 2 && board[catchy][catchx] >= 0)
 	{
 		return 1; 
 	}
-	if (oldy + 2 < BOARDLEN && oldx + 1 < BOARDLEN && newy == oldy + 2 && newx == oldx + 1 && board[newy][newx] >= 0)
+	if (oldy + 2 < BOARDLEN && oldx + 1 < BOARDLEN && catchy == oldy + 2 && catchx == oldx + 1 && board[catchy][catchx] >= 0)
 	{
 		return 1; 
 	}
-	if (oldy + 2 < BOARDLEN && oldx - 1 >= 0 && newy == oldy + 2 && newx == oldx - 1 && board[newy][newx] >= 0)
+	if (oldy + 2 < BOARDLEN && oldx - 1 >= 0 && catchy == oldy + 2 && catchx == oldx - 1 && board[catchy][catchx] >= 0)
 	{
 		return 1; 
 	}
-	if (oldy + 1 < BOARDLEN && oldx - 2 >= 0 && newy == oldy + 1 && newx == oldx - 2 && board[newy][newx] >= 0)
+	if (oldy + 1 < BOARDLEN && oldx - 2 >= 0 && catchy == oldy + 1 && catchx == oldx - 2 && board[catchy][catchx] >= 0)
 	{
 		return 1; 
 	}
-	if (oldy - 1 >= 0 && oldx - 2 >= 0 && newy == oldy - 1 && newx == oldx - 2 && board[newy][newx] >= 0)
+	if (oldy - 1 >= 0 && oldx - 2 >= 0 && catchy == oldy - 1 && catchx == oldx - 2 && board[catchy][catchx] >= 0)
 	{
 		return 1;
 	}
 	return 0;
 }
 
-int KnightBlackCatch(int oldx, int oldy, int newx, int newy)
+int KnightBlackCatch(int oldx, int oldy, int catchx, int catchy)
 {
-	if (oldy - 2 >= 0 && oldx - 1 >= 0 && newy == oldy - 2 && newx == oldx - 1 && board[newy][newx] <= 0)
+	if (oldy - 2 >= 0 && oldx - 1 >= 0 && catchy == oldy - 2 && catchx == oldx - 1 && board[catchy][catchx] <= 0)
 	{
 		return 1;
 	}
-	if (oldy - 2 >= 0 && oldx + 1 < BOARDLEN && newy == oldy - 2 && newx == oldx + 1 && board[newy][newx] <= 0)
+	if (oldy - 2 >= 0 && oldx + 1 < BOARDLEN && catchy == oldy - 2 && catchx == oldx + 1 && board[catchy][catchx] <= 0)
 	{
 		return 1; 
 	}
-	if (oldy - 1 >= 0 && oldx + 2 < BOARDLEN && newy == oldy - 1 && newx == oldx + 2 && board[newy][newx] <= 0)
+	if (oldy - 1 >= 0 && oldx + 2 < BOARDLEN && catchy == oldy - 1 && catchx == oldx + 2 && board[catchy][catchx] <= 0)
 	{
 		return 1; 
 	}
-	if (oldy + 1 >= 0 && oldx + 2 < BOARDLEN && newy == oldy + 1 && newx == oldx + 2 && board[newy][newx] <= 0)
+	if (oldy + 1 >= 0 && oldx + 2 < BOARDLEN && catchy == oldy + 1 && catchx == oldx + 2 && board[catchy][catchx] <= 0)
 	{
 		return 1; 
 	}
-	if (oldy + 2 < BOARDLEN && oldx + 1 < BOARDLEN && newy == oldy + 2 && newx == oldx + 1 && board[newy][newx] <= 0)
+	if (oldy + 2 < BOARDLEN && oldx + 1 < BOARDLEN && catchy == oldy + 2 && catchx == oldx + 1 && board[catchy][catchx] <= 0)
 	{
 		return 1; 
 	}
-	if (oldy + 2 < BOARDLEN && oldx - 1 >= 0 && newy == oldy + 2 && newx == oldx - 1 && board[newy][newx] <= 0)
+	if (oldy + 2 < BOARDLEN && oldx - 1 >= 0 && catchy == oldy + 2 && catchx == oldx - 1 && board[catchy][catchx] <= 0)
 	{
 		return 1; 
 	}
-	if (oldy + 1 < BOARDLEN && oldx - 2 >= 0 && newy == oldy + 1 && newx == oldx - 2 && board[newy][newx] <= 0)
+	if (oldy + 1 < BOARDLEN && oldx - 2 >= 0 && catchy == oldy + 1 && catchx == oldx - 2 && board[catchy][catchx] <= 0)
 	{
 		return 1; 
 	}
-	if (oldy - 1 >= 0 && oldx - 2 >= 0 && newy == oldy - 1 && newx == oldx - 2 && board[newy][newx] <= 0)
+	if (oldy - 1 >= 0 && oldx - 2 >= 0 && catchy == oldy - 1 && catchx == oldx - 2 && board[catchy][catchx] <= 0)
 	{
 		return 1;
 	}
