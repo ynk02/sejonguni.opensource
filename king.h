59ed72573c1a520abb2bee7c3af9e7bce3326b82
jehhuilee <dlwpgml56@naver.com>
2024-06-07 12:12:54 +0900

    킹 블랙 잡혔을 때 추가

--- king.h
+++ king.h
@@ -43,3 +43,40 @@ int KingWhiteCatch(int beforeX, int beforeY, int afterX, int afterY)
 	}
 	return 0;
 }
+
+int KingBlackCatch(int beforeX, int beforeY, int afterX, int afterY)
+{
+	if (beforeX - 1 >= 0 && beforeY - 1 >= 0 && afterY == beforeY - 1 && afterX == beforeX - 1 && board[afterY][afterX] >= 0)
+	{
+		return 1;
+	}
+	if (beforeY - 1 >= 0 && afterX == beforeX && afterY == beforeY - 1 && board[afterY][afterX] >= 0)
+	{
+		return 1;
+	}
+	if (beforeY - 1 >= 0 && beforeX + 1 < BOARDLEN && afterX == beforeX + 1 && afterY == beforeY - 1 && board[afterY][afterX] >= 0)
+	{
+		return 1;
+	}
+	if (beforeX + 1 < BOARDLEN && afterY == beforeY && afterX == beforeX + 1 && board[afterY][afterX] >= 0)
+	{
+		return 1;
+	}
+	if (beforeX + 1 < BOARDLEN && beforeY + 1 < BOARDLEN && afterY == beforeY + 1 && afterX == beforeX + 1 && board[afterY][afterX] >= 0)
+	{
+		return 1;
+	}
+	if (beforeY + 1 < BOARDLEN && afterY == beforeY + 1 && afterX == beforeX && board[afterY][afterX] >= 0)
+	{
+		return 1;
+	}
+	if (beforeX - 1 >= 0 && beforeY + 1 < BOARDLEN && afterX == beforeX - 1 && afterY == beforeY + 1 && board[afterY][afterX] >= 0)
+	{
+		return 1;
+	}
+	if (beforeX - 1 >= 0 && afterY == beforeY && afterX == beforeX - 1 && board[afterY][afterX] >= 0)
+	{
+		return 1;
+	}
+	return 0;
+}
