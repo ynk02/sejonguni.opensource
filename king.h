a83bf515c7b7552908b64264f223e9bdf18bfaa2
ossys0920 <ossys0920@gmail.com>
2024-06-07 12:35:16 +0900

    KingWhiteMove(캐슬링제외) 추가

--- king.h
+++ king.h
@@ -238,6 +238,73 @@ int KingBlackMove(int oldx, int oldy, int newx, int newy)
 	}
 }
 
+int KingWhiteMove(int oldx, int oldy, int newx, int newy)
+{
+	if (oldx - 1 >= 0 && oldy - 1 >= 0 && newy == oldy - 1 && newx == oldx - 1 && board[newy][newx] >= 0)
+	{
+		int ok = KingWhiteCheck(oldx - 1, oldy - 1);
+		if (ok == 1)
+		{
+			return 1;  
+		}
+	}
+	if (oldy - 1 >= 0 && newx == oldx && newy == oldy - 1 && board[newy][newx] >= 0)
+	{
+		int ok = KingWhiteCheck(oldx, oldy - 1);
+		if (ok == 1)
+		{
+			return 1; 
+		}
+	}
+	if (oldy - 1 >= 0 && oldx + 1 < BOARDLEN && newx == oldx + 1 && newy == oldy - 1 && board[newy][newx] >= 0)
+	{
+		int ok = KingWhiteCheck(oldx + 1, oldy - 1);
+		if (ok == 1)
+		{
+			return 1; 
+		}
+	}
+	if (oldx + 1 < BOARDLEN && newy == oldy && newx == oldx + 1 && board[newy][newx] >= 0)
+	{
+		int ok = KingWhiteCheck(oldx + 1, oldy);
+		if (ok == 1)
+		{
+			return 1; 
+		}
+	}
+	if (oldx + 1 < BOARDLEN && oldy + 1 < BOARDLEN && newy == oldy + 1 && newx == oldx + 1 && board[newy][newx] >= 0)
+	{
+		int ok = KingWhiteCheck(oldx + 1, oldy + 1);
+		if (ok == 1)
+		{
+			return 1; 
+		}
+	}
+	if (oldy + 1 < BOARDLEN && newy == oldy + 1 && newx == oldx && board[newy][newx] >= 0)
+	{
+		int ok = KingWhiteCheck(oldx, oldy + 1);
+		if (ok == 1)
+		{
+			return 1; 
+		}
+	}
+	if (oldx - 1 >= 0 && oldy + 1 < BOARDLEN && newx == oldx - 1 && newy == oldy + 1 && board[newy][newx] >= 0)
+	{
+		int ok = KingWhiteCheck(oldx - 1, oldy + 1);
+		if (ok == 1)
+		{
+			return 1; 
+		}
+	}
+	if (oldx - 1 >= 0 && newy == oldy && newx == oldx - 1 && board[newy][newx] >= 0)
+	{
+		int ok = KingWhiteCheck(oldx - 1, oldy);
+		if (ok == 1)
+		{
+			return 1; 
+		}
+	}
+}
 
 
 
