20e56a46d31929e7845182ff61d65644c4ef9df4
ynk02 <namuk0225@naver.com>
2024-06-07 12:19:51 +0900

    킹 체크를 확인하는 코드 추가

--- king.h
+++ king.h
@@ -80,3 +80,93 @@ int KingBlackCatch(int beforeX, int beforeY, int afterX, int afterY)
 	}
 	return 0;
 }
+
+//화이트킹 체크 확인
+int KingWhiteCheck(int dx, int dy)
+{
+	int ok = 0;
+	for (int i = 0; i < BOARDLEN; i++)
+	{
+		for (int j = 0; j < BOARDLEN; j++)
+		{
+			if (board[i][j] > 0)
+			{
+				if (board[i][j] == PawnB)
+				{
+					ok = PawnBlackCatch(j, i, dx, dy);
+				}
+				if (board[i][j] == RookB)
+				{
+					ok = RookBlackCatch(j, i, dx, dy);
+				}
+				if (board[i][j] == KnightB)
+				{
+					ok = KnightBlackCatch(j, i, dx, dy);
+				}
+				if (board[i][j] == BishopB)
+				{
+					ok = BishopBlackCatch(j, i, dx, dy);
+				}
+				if (board[i][j] == QueenB)
+				{
+					ok = QueenBlackCatch(j, i, dx, dy);
+				}
+				if (board[i][j] == KingB)
+				{
+					ok = KingBlackCatch(j, i, dx, dy);
+				}
+				if (ok == 1)
+				{
+					//	std::cout << "da" << "\n";
+					return 0;
+				}
+			}
+		}
+	}
+	return 1;
+}
+
+
+//블랙킹 체크 확인
+int KingBlackCheck(int dx, int dy)
+{
+	int ok = 0;
+	for (int i = 0; i < BOARDLEN; i++)
+	{
+		for (int j = 0; j < BOARDLEN; j++)
+		{
+			if (board[i][j] < 0)
+			{
+				if (board[i][j] == PawnW)
+				{
+					ok = PawnWhiteCatch(j, i, dx, dy);
+				}
+				if (board[i][j] == RookW)
+				{
+					ok = RookWhiteCatch(j, i, dx, dy);
+				}
+				if (board[i][j] == KnightW)
+				{
+					ok = KnightWhiteCatch(j, i, dx, dy);
+				}
+				if (board[i][j] == BishopW)
+				{
+					ok = BishopWhiteCatch(j, i, dx, dy);
+				}
+				if (board[i][j] == QueenW)
+				{
+					ok = QueenWhiteCatch(j, i, dx, dy);
+				}
+				if (board[i][j] == KingW)
+				{
+					ok = KingWhiteCatch(j, i, dx, dy);
+				}
+				if (ok == 1)
+				{
+					return 0;
+				}
+			}
+		}
+	}
+	return 1;
+}
